89ccf007ba0d125c9ae373fbfa8c366158331ce7
Zomry <mathias.craps@student.ehb.be>
2015-04-17 02:56:51 +0200

    More updates
    Added support for input and area, inspired by NVDA. Cleaned the code up
    a bit. Still need to fix
    the issue that disables using the e-key for navigating through edit
    fields.
    Updated some names to conform to the NVDA-implementation /
    ARIA-standard.
    
    More changes to follow.

--- screen-lib.js
+++ screen-lib.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+	var voiceIsOn = false;
 	var voices = window.speechSynthesis.getVoices();
 	var arrayTypes = {}; // caching-container to keep the adequate elements of the types
 
@@ -7,6 +8,8 @@ window.onload = function() {
 		mode: null,
 		lastNode: null
 	}
+
+	//document.getElementById("fake-button").attributes["aria-role"]
 	
 	var keys = {
 		h: 72, // to next header
@@ -25,15 +28,26 @@ window.onload = function() {
 		q: 81, // to next block quote
 		s: 83, // to next seperator
 		m: 77, // to next frame
-		g: 71 //  to next graphic
+		g: 71, //  to next graphic,
+		1: 97,
+		2: 98,
+		3: 99,
+		4: 100,
+		5: 101,
+		6: 102
 	};
 
 	types("a");
-	types("button");
+	types("button", "button");
 	types("ul");
 	types("li");
-	types("input");
+	types("input, textarea");
 	types("img");
+	types("h1", "header");
+	types("h2", "header");
+	types("h3", "header");
+	// types("div");
+
 	document.body.onkeyup = navigate;
 
 	function navigate(e) {
@@ -44,6 +58,23 @@ window.onload = function() {
 			case keys.i: // list item
 				switchElem("li", "li", e);
 				break;
+			case keys.b: // button
+				switchElem("button", "button", e);
+				break;
+			case keys.g:
+				switchElem("img", "img", e);
+				break;
+			case keys.e:
+				switchElem("input, textarea", "input", e);
+				break;
+			case keys["1"]:
+			case keys["2"]:
+			case keys["3"]:
+			case keys["4"]:
+			case keys["5"]:
+			case keys["6"]:
+				switchElem("header", "H" + e.which, e);
+			// etc
 		}
 	}
 
@@ -57,15 +88,17 @@ window.onload = function() {
 			? arrayTypes[type][current.index-1]
 			: arrayTypes[type][current.index+1]
 
-		console.log(nextNode);
-
 		if(nextNode != undefined) {
 			(e.shiftKey)
 				? current.index--
 				: current.index++;
 			current.lastNode = nextNode;
-			nextNode.tabIndex = -999; // still needs to be reset to preview value if necessary
+			nextNode.tabIndex = -1; // still needs to be reset to preview value if necessary
 			nextNode.focus();
+		} else {
+			(e.shiftKey) 
+				? handleSpeaking("No previous " + newMode)
+				: handleSpeaking("No next " + newMode);
 		}
 	}
 
@@ -76,13 +109,22 @@ window.onload = function() {
 			c.focus();
 	}
 
-	function types(selector) {
-		var all;
+	function types(selector, ariaRole) {
+		var all, query = "";
+		var all2;
 
 		if(typeof selector == "string") {
-			all = document.body.querySelectorAll(selector);
+			query += selector;
+			if(ariaRole) { // if set
+				query += ", [aria-role=" + ariaRole + "]";
+			}
+
+			all = document.body.querySelectorAll(query);
 			all = Array.prototype.slice.call(all); //  convert to real array
-		} else if (typeof selector == "array") { // impossible condition, but temporary placeholder
+
+		}
+
+		if (typeof selector == "array") { // impossible condition, but temporary placeholder
 
 		}
 
@@ -96,70 +138,129 @@ window.onload = function() {
 	}
 
 	function synthesize(e) {
+		handleSpeaking(elemContent(e.path[0]));
+	}
+
+	function handleSpeaking(message) {
+
 		if(speechSynthesis.speaking) {
 			speechSynthesis.cancel()
 		}
+
 		var msg = new SpeechSynthesisUtterance();
 		msg.voice = voices[4];
 		msg.voiceURI = 'native';
 		msg.volume = 1; // 0 to 1
 		msg.rate = 1; // 0.1 to 10
 		msg.pitch = 2; //0 to 2
-		msg.text = elemName(e.path[0]) + ". " + elemContent(e.path[0]);
+		msg.text = message;
 
 		msg.lang = 'en-UK';
-		speechSynthesis.speak(msg);
-		speechSynthesis.resume();
+
+		if(voiceIsOn) {
+			speechSynthesis.speak(msg);
+			speechSynthesis.resume();	
+		}		
+		console.log(message);
 	}
 
-	function elemName(elem) {
-		var name = elem.nodeName;
-		switch(name) {
-			case "A":
-				return "Anchor";
-			case "INPUT":
-				return "Input " + elem.type || "Text";
-			case "LI":
-				return "List item";
-			case "IMG":
-				return "Image";
-			case "BUTTON":
-				return "Button";
-			case "UL":
-				return "Unordered list";
-			case "OL":
-				return "Ordered list";
-		}
-
-		return name; // fallback if not defined
+	var objNames = {
+		"A": "link",
+		"INPUT": "edit",
+		"TEXTAREA": "edit",
+		"IMG": "graphic",
+		"BUTTON": "button",
+		"LI": "list item",
+		"UL": "Unorder list",
+		"OL": "Ordered list",
+		"1": "one",
+		"2": "two",
+		"3": "three", 
+		"4": "four",
+		"5": "five",
+		"6": "six"
 	}
 
 	function elemContent(elem) {
-		if(elem.nodeName == "INPUT") {
-			console.log("lalala");
-			return elem.placeholder;
+
+		if(elem.nodeName == "INPUT" || elem.nodeName == "TEXTAREA") {
+			var text = "";
+
+			if(elem.id) { // if has id
+				var label = document.querySelector("label[for=" + elem.id + "]");
+				if(label) { // and have assistive label
+					text += label.innerText + " "; // then add description
+				}
+			}
+
+			text += objNames["INPUT"] + " ";
+
+
+			text += (elem.disabled) ? "unavailable " : " "; // if disabled
+			text += elem.placeholder + " ";
+			text += elem.value + ""; // 
+			return text;
 		}
 
 		if(elem.nodeName == "UL" || elem.nodeName == "OL") {
-			return elem.getElementsByTagName("li").length + " items";
+			return objNames[elem.nodeName] + " " + elem.getElementsByTagName("li").length + " items";
 		}
 
 
 		if(elem.nodeName == "IMG") {
+			var val = objNames["IMG"] + " ";
+
 			if(elem.alt != "") 	{
-				return elem.alt;
+				val += elem.alt + ".";
 			} else {
 				var src = elem.src;
 
 				if(src.indexOf('/') >= 0) {
-				   src = src.substring(src.lastIndexOf('/')+1);
+				   src = src.substring(src.lastIndexOf('/')+1) + " ";
 				}
-				return src;
+
+				val += src;
 			}
+
+			val += (elem.longDesc != "") ? " Description available. Press enter to listen." : " ";
+			return val;
+
+			// if(elem.alt != "") 	{
+			// 	return elem.alt;
+			// } else {
+			// 	var src = elem.src;
+
+			// 	if(src.indexOf('/') >= 0) {
+			// 	   src = src.substring(src.lastIndexOf('/')+1);
+			// 	}
+
+			// 	src += (elem.longDesc != "") 
+			// 	? " description available" : " no description available";
+			// 	return src;
+			// }
 		}
 
 		if(elem.nodeName == "A") {
-			return elem.title + elem.innerText;
+			var href = objNames[elem.nodeName] + " " + elem.href;
+
+				if(href.indexOf('/') >= 0) {
+				   href = href.substring(href.lastIndexOf('/')+1) ;
+				}
+			return elem.title + elem.innerText + " location " + href;
+		}
+
+		if(elem.nodeName == "BUTTON") {
+			return objNames[elem.nodeName] + " " + elem.innerText;
+		}
+
+		switch (elem.nodeName) {
+			case "H1":
+			case "H2":
+			case "H3":
+			case "H4":
+			case "H5":
+			case "H6":
+				return "Header level " + objNames[elem.nodeName.substring(2, 1)] + " " + elem.innerText; 
 		}
 
 		if(elem.outerText != undefined || elem.outerText == "") {
